c05a5d213f6dd7650c217b17a7e0c75e63882f1f
Mario Kempf | LOGMEDIA <kempf@logmedia.at>
2015-06-30 10:32:19 +0200

    text

--- module/Application/src/Application/Controller/FacebookController.php
+++ module/Application/src/Application/Controller/FacebookController.php
@@ -21,7 +21,7 @@ class FacebookController extends AbstractActionController
             $metaHelper->setProperty("og:image","http://www.carrybase.com/img/sea.jpg");
             $metaHelper->setProperty("og:title","TRAUMURLAUB - Ägypten TOP 4* Hotel All Inclusive - 1 Woche - ab 317€");
             $metaHelper->setProperty("og:type","article");
-            $metaHelper->setProperty("og:description","1 Woche im TOP 4* Hotel in Ägypten / Hurghada mit Strandnähe, 6* Bewertung auf Holidaytest, All Inclusive, inkl. Flug + Transfer, diverse österreichische Abflughäfen - Überzeugt euch selbst und teilt dieses Schnäppchen!");
+            $metaHelper->setProperty("og:description","1 Woche im TOP 4* Hotel in Ägypten / Hurghada mit Strandnähe, 6* Bewertung auf Holidaytest, All Inclusive, diverse österreichische Abflughäfen - Überzeugt euch selbst und teilt dieses Schnäppchen!");
         }
         else{
             return $this->redirect()->toUrl("https://ad.zanox.com/ppc/?31868007C76436087&ULP=[[http://www.ab-in-den-urlaub.de/ibe/offers/page/1/params/tt/adult/2/agent/ab-in-den-urlaub.de/area/350/depAirport/19%2C23%2C30%2C42%2C46/depDate/30.06.2015/dest/13/distribution/CH/duration/6_7/optCategory/2/optMeal/4/port/654/regiontreeSource/unister/retDate/09.08.2015/totalAdultChildCount/2/hotelId/10785/topHotelSelected/0/start/0/allGermAp/0/allSwissAp/0/ibecat/lastminute/route/flattrip/formSelected/lastminute/switchDestinationField/input/hotelIdType/iff/sortStep3/price/orderdirStep3/asc/hotelAttributesWellness/-1/hotelAttributesSport/-1/hotelAttributes/-1/nottot/1]]");
